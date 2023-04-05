efc72d0c62bc4a1abd426719f3d0af2129f13c83
SryNext <cleansoftwaresolution01@gmail.com>
2023-04-05 16:35:24 +0200

    reworked code for better performance.

--- therapist.py
+++ therapist.py
@@ -1,10 +1,17 @@
 # Import necessary libraries
 import random
 
-# Create a list of potential responses to user input
-responses = ["I'm sorry to hear that. How are you feeling right now?",
-             "That sounds really tough. Would you like to talk about it?",
-             "Thank you for sharing that with me. How can I support you?"]
+# Create a dictionary of potential responses to user input
+responses = {"default": ["I'm sorry to hear that. How are you feeling right now?",
+                         "That sounds really tough. Would you like to talk about it?",
+                         "Thank you for sharing that with me. How can I support you?"],
+             "hotline": ["Here is a list of hotlines you can call for immediate assistance:",
+                         "1. National Suicide Prevention Lifeline: 1-800-273-TALK (8255)",
+                         "2. Crisis Text Line: Text HOME to 741741"],
+             "resource": ["Here are some resources that might be helpful to you:",
+                          "1. National Alliance on Mental Illness (NAMI): https://www.nami.org/",
+                          "2. Substance Abuse and Mental Health Services Administration (SAMHSA): https://www.samhsa.gov/"],
+             "suicide": ["Suicide is not a Way"]}
 
 # Define a function to handle the therapy session
 def virtual_therapist():
@@ -13,7 +20,7 @@ def virtual_therapist():
     user_input = input("Hello, how can I assist you today? ")
     
     # Provide a supportive response to the user's input
-    print(random.choice(responses))
+    print(random.choice(responses["default"]))
     
     # While loop to continue the conversation until the user is ready to end it
     while True:
@@ -26,17 +33,16 @@ def virtual_therapist():
             break
             
         elif "hotline" in user_input.lower():
-            print("Here is a list of hotlines you can call for immediate assistance:")
-            print("1. National Suicide Prevention Lifeline: 1-800-273-TALK (8255)")
-            print("2. Crisis Text Line: Text HOME to 741741")
+            for response in responses["hotline"]:
+                print(response)
             
         elif "resource" in user_input.lower():
-            print("Here are some resources that might be helpful to you:")
-            print("1. National Alliance on Mental Illness (NAMI): https://www.nami.org/")
-            print("2. Substance Abuse and Mental Health Services Administration (SAMHSA): https://www.samhsa.gov/")
+            for response in responses["resource"]:
+                print(response)
         
         elif "suicide" in user_input.lower():
-            print("Suicide is not a Way")
+            for response in responses["suicide"]:
+                print(response)
             
         else:
             print("I hear you. Can you tell me more about what you're experiencing?")
